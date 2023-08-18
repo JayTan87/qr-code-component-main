db09b2f6baeb964ff7e2170c1d062f175970bdf8
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-08-18 09:10:29 -0500

    updated gitignore

---
+++ database-exercises/albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name  VARCHAR(100) NOT NULL,
+    release_date INT,
+    sales INT,
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
+);
