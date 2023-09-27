f4a9d684dcd18c5aa1f020081e5054e51565ad0b
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-09-27 14:50:41 -0500

    updated file with example.properties

---
+++ BOTTS/src/main/java/com/example/botts/BottsApplication.java
@@ -0,0 +1,13 @@
+package com.example.botts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BottsApplication.class, args);
+    }
+
+}
---
+++ BOTTS/src/test/java/com/example/botts/BottsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
